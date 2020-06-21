e6ecb1a5dd9020219abf522e0ea3cc4362abbab7
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2020-06-21 19:47:20 +0100

    get request for recent repositories

--- dmg/src/components/Projects.js
+++ dmg/src/components/Projects.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Project from "./Project";
+import request from "request";
 
 class Projects extends Component {
   state = {
@@ -8,10 +9,26 @@ class Projects extends Component {
   };
 
   componentDidMount() {
+    const clientId = "e49b037dba7346b4c870";
+    const secret = "70a4b6b57f85ce2d139e803e83e45472f584bd2a";
+    console.log(secret);
+    const options = {
+      uri: `https://api.github.com/users/denriquem/repos?per_page=5&sort=created: asc&client_id${clientId}&client_secret${secret}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      const res1 = response.toJSON(JSON.parse(body));
+      console.log(res1.body);
+    });
+
     axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
       const projectsShort = res.data.slice(0, 4);
       this.setState({ projects: projectsShort });
-      console.log(projectsShort);
+      // console.log(projectsShort);
     });
   }
 
