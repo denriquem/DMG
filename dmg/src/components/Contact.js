1267d63e8ea63f87e1b86428896c886eeaa54a70
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2020-06-26 17:58:23 +0100

    style; email icon

--- dmg/src/components/Contact.js
+++ dmg/src/components/Contact.js
@@ -5,6 +5,8 @@ export const Contact = () => {
     <div className="contact">
       <div className="contactHeader light-text">Contact</div>
       <div className="textBox3 main-text light-text">
+        <i class="fa fa-envelope" aria-hidden="true"></i>
+        {"   "}
         demartinezgatell@gmail.com
       </div>
     </div>
